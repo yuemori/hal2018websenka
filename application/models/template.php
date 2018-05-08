2261621e35bef4405f264fb2b2fa87e1795f139e
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-08 11:36:11 +0900

    モデルクラスを生成するスクリプトの追加

---
+++ application/models/template.php
@@ -0,0 +1,22 @@
+<?php
+/*!
+ * TABLENAME_model
+ *
+ */
+	class TABLENAME_model extends CI_Model
+	{
+		/*!
+		 */
+		public function __construct()
+		{
+			$this->load->database();
+		}
+
+		/*!
+		 */
+		public function create()
+		{
+		}
+	}
+
+?>
