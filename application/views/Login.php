8e77fb9f08c33fba37f113cf8d6a6e92601bf576
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-09 21:41:42 +0900

    トップページの雛形を追加

---
+++ application/views/Login.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <link href="<?=base_url();?>css/default.css" type="text/css" rel="stylesheet" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Login</title>
+  </head>
+  <body>
+    <p class="alert">Login test view </p>
+    このファイルは自動生成されました。<br />
+
+	<a href="<?=site_url('ModeSelect');?>">ログイン</a><br />
+
+	<a href="<?=site_url('Regist');?>">新規登録</a><br />
+  </body>
+</html>
