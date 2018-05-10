8a009227b43d4f9d079fc11b1be1d0a3a93cddf0
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-10 11:28:14 +0900

    ログイン周りのページ遷移をパスワード評価ページを挟む様に変更

--- application/controllers/LoginCheck.php
+++ application/controllers/LoginCheck.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class LoginCheck extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,9 +20,6 @@ class Login extends CI_Controller {
 	 */
 	public function index()
 	{
-		// クリックジャッキング対策
-		header('X-FRAME-OPTIONS: SAMEORIGIN');
-
-		$this->load->view('Login.php');
+		redirect("ModeSelect");
 	}
 }
--- application/views/Login.php
+++ application/views/Login.php
@@ -8,7 +8,7 @@
     <p class="alert">Login test view </p>
     このファイルは自動生成されました。<br />
 
-	<a href="<?=site_url('ModeSelect');?>">ログイン</a><br />
+	<a href="<?=site_url('LoginCheck');?>">ログイン</a><br />
 
 	<a href="<?=site_url('Regist');?>">新規登録</a><br />
   </body>
