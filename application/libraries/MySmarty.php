bbe46953be8b4be2fa09d386f992138e8f78f89c
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-10 15:52:51 +0900

    Codeigniterの初期設定：Smartyを使える様に準備
    　・Smartyのインストールに必要な設定を記述
    　・Codeigniter実行時にSmartyライブラリが読み込まれる様に設定変更
    　・Smartyエンジンの初期化
    　・CodeigniterのController内からアクセスできる様に、
    　　グローバルなライブラリとして操作クラスを定義
    　・テンプレートファイルを置く場所を用意
    　・テンプレートキャッシュが出力される場所を用意
    　・.gitignoreにテンプレートキャッシュファイルを追加
    　・よく使う定数をテンプレートに渡すようにした
    
    実際のインストールは、このコミットを取り込んだ後、下記コマンドを実行する
    -----------------------
    > cd application
    > php ../composer.phar require smarty/smarty

---
+++ application/libraries/MySmarty.php
@@ -0,0 +1,62 @@
+<?php
+/*!
+ * テンプレートエンジンSmartyを提供するクラス
+ *
+ * Smarty本体はcomposerでインストールされ "vendor/smarty/smarty/" にあります。
+ * smarty/libs/Smarty.class.php は config.php の composer_autoload によって
+ * 既に利用できる状態になっている前提です。
+ *
+ * もし、このファイルでエラーになる場合は、以下を試してみて下さい。
+ * > cd application
+ * > php ../composer.phar update
+ * > php ../composer.phar install
+ *
+ */
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+//require_once FCPATH. '/vendor/smarty/smarty/libs/Smarty.class.php';
+
+class MySmarty extends Smarty
+{
+    public function __construct()
+    {
+		parent::__construct();
+
+        $this->compile_dir = APPPATH . "views/templates_c";
+        $this->template_dir = APPPATH . "views/templates";
+        $this->assign('APPPATH', APPPATH);
+        $this->assign('BASEPATH', BASEPATH);
+
+        // Assign CodeIgniter object by reference to CI
+        if (method_exists($this, 'assignByRef')) {
+			$ci =& get_instance();
+			$this->assignByRef("ci", $ci);
+		}
+        log_message('debug', "Smarty Class Initialized");
+    }
+
+    function view($template, $data = array(), $return = FALSE)
+    {
+		$data['BASE_URL'] = base_url(). "/";
+		$data['SITE_URL'] = site_url(). "/";
+		$data['CURRENT_URL'] = current_url();
+		$data['INDEX_PAGE'] = index_page();
+        foreach ($data as $key => $val) {
+			$this->assign($key, $val);
+		}
+
+        if ($return == FALSE) {
+			$CI =& get_instance();
+			if (method_exists($CI->output, 'set_output')) {
+				$CI->output->set_output($this->fetch($template));
+			} else {
+				$CI->output->final_output = $this->fetch($template);
+			}
+			return "";
+		} else {
+			return $this->fetch($template);
+		}
+		return "";
+    }
+}
+?>
