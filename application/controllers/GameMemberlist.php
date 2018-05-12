d6204d1dca88e6567457e0a9784acadb1b943d22
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-12 15:19:14 +0900

    仮組み：ゲームに参加中の人一覧を表示するページの追加

---
+++ application/controllers/GameMemberlist.php
@@ -0,0 +1,43 @@
+<?php
+/*!
+ * ゲーム画面
+ *
+ * user_id: ログイン中のユーザーID
+ * game_id: 進行中のゲームID
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class GameMemberlist extends CI_Controller
+{
+	var $_user_id;
+	var $_game_id;
+
+	private function _validate()
+	{
+   		$this->_user_id = $this->input->get('user_id');
+		if (NULL == $this->_user_id) {
+			throw new Exception('invalid params user_id');
+		}
+		$this->_game_id = $this->input->get('game_id');
+		if (NULL == $this->_game_id) {
+			throw new Exception('invalid params game_id');
+		}
+	}
+
+	public function index()
+	{
+		try {
+			$this->_validate();
+		} catch (Exception $obj) {
+			var_dump($obj);
+		}
+		echo sprintf("user_id:%s", $this->_user_id);
+		echo sprintf("game_id:%s", $this->_game_id);
+
+		$data["user_id"] = $this->_user_id;
+		$data["game_id"] = $this->_game_id;
+		$this->smarty->view("GameMemberlist.tpl", $data);
+	}
+
+}
