d6f79f8660b78fabb952b00a19c90b3148865dc7
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-15 13:13:12 +0900

    ユーザーからの発言を処理するページの修正
    ・ちょっと読みにくくなってしまっていたのでコードを整理
    ・Game_model::loadが、他モデルクラスのload()とI/Fが違っていたので、他に合せる形で仕様変更

--- application/models/Gamelog_model.php
+++ application/models/Gamelog_model.php
@@ -30,10 +30,14 @@ require_once dirname(__FILE__). '/Game_model.php';
 
 		/*!
 		 */
-		public function write($game_model, $user_id, $message)
+		public function write($game, $user_id, $message)
 		{
-			$this->db->set('game_id', $game_model->id());
-			$this->db->set('wave', $game_model->wave());
+			if (!isset($game)) {
+				return false;
+			}
+
+			$this->db->set('game_id', $game->game_id);
+			$this->db->set('wave', $game->wave);
 			$this->db->set('user_id', $user_id);
 			$this->db->set('say', $message);
 			$this->db->set('insert_at', 'NOW()', FALSE);
