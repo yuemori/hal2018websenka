348c43073dc1158cd99bdf512752c0dbc96ecd66
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-05-21 15:45:10 +0900

    modify testdata sql

--- application/models/testdata.sql
+++ application/models/testdata.sql
@@ -40,9 +40,9 @@ VALUES
 
 DELETE FROM Game;
 INSERT INTO Game
-  (game_id, wave, status, group_id, minority_user_id, minimum)
+  (game_id, wave, max_wave, status, group_id, minority_user_id, minimum)
 VALUES
-  (1, 0, 0, 1, 1, 4)
+  (1, 4, 4, 0, 1, 1, 4)
 ;
 
 
@@ -62,21 +62,26 @@ DELETE FROM GameLog;
 INSERT INTO GameLog
   (log_id, game_id, wave, user_id, say, insert_at, update_at)
 VALUES
-  (1,  1, 1, 1, '結構良く食卓でみかける？', NOW(), NOW())
-, (2,  1, 1, 2, 'そうだね。よく見かけるかな？', NOW()+1, NOW())
-, (3,  1, 1, 3, 'そうだね', NOW()+2, NOW())
-, (4,  1, 1, 4, 'うんうん', NOW()+3, NOW())
-, (5,  1, 1, 5, '一週間に一回は見るかな？', NOW()+4, NOW())
+  (1,  1, 1, 1, '結構良く食卓でみかける？', (NOW() +  INTERVAL 0 SECOND), NOW())
+, (2,  1, 1, 2, 'そうだね。よく見かけるかな？', (NOW() + INTERVAL 1 SECOND), NOW())
+, (3,  1, 1, 3, 'そうだね', (NOW() + INTERVAL 2 SECOND), NOW())
+, (4,  1, 1, 4, 'うんうん', (NOW() + INTERVAL 3 SECOND), NOW())
+, (5,  1, 1, 5, '一週間に一回は見るかな？', (NOW() + INTERVAL 4 SECOND), NOW())
 
-, (6,  1, 2, 1, 'ほげ', NOW()+10, NOW())
-, (7,  1, 2, 2, 'ふが', NOW()+11, NOW())
-, (8,  1, 2, 3, '何気にコレって気持ち悪くない？', NOW()+12, NOW())
-, (9,  1, 2, 4, 'そうっすねー', NOW()+13, NOW())
-, (10, 1, 2, 5, '<font color="red">( ﾟдﾟ)ﾎﾟｶｰﾝ</font>', NOW()+14, NOW())
+, (6,  1, 2, 1, 'ほげ', (NOW() + INTERVAL 10 SECOND), NOW())
+, (7,  1, 2, 2, 'ふが', (NOW() + INTERVAL 11 SECOND), NOW())
+, (8,  1, 2, 3, '何気にコレって気持ち悪くない？', (NOW() + INTERVAL 12 SECOND), NOW())
+, (9,  1, 2, 4, 'そうっすねー', (NOW() + INTERVAL 13 SECOND), NOW())
+, (10, 1, 2, 5, '<font color="red">( ﾟдﾟ)ﾎﾟｶｰﾝ</font>', (NOW() + INTERVAL 14 SECOND), NOW())
 
-, (11, 1, 3, 1, 'てすとー', NOW()+20, NOW())
-, (12, 1, 3, 2, 'こめんとー', NOW()+21, NOW())
-, (13, 1, 3, 3, 'テストデータ作るの疲れた', NOW()+22, NOW())
-, (14, 1, 3, 4, 'そうだね、疲れるよねこれ', NOW()+23, NOW())
-, (15, 1, 3, 5, 'もうええんちゃうかな？', NOW()+24, NOW())
+, (11, 1, 3, 1, 'てすとー', (NOW() + INTERVAL 20 SECOND), NOW())
+, (12, 1, 3, 2, 'こめんとー', (NOW() + INTERVAL 21 SECOND), NOW())
+, (13, 1, 3, 3, 'テストデータ作るの疲れた', (NOW() + INTERVAL 22 SECOND), NOW())
+, (14, 1, 3, 4, 'そうだね、疲れるよねこれ', (NOW() + INTERVAL 23 SECOND), NOW())
+, (15, 1, 3, 5, 'もうええんちゃうかな？', (NOW() + INTERVAL 24 SECOND), NOW())
+
+, (22, 1, 4, 2, 'テスト１が発言したら', (NOW() + INTERVAL 31 SECOND), NOW())
+, (23, 1, 4, 3, 'このターンも終わりで', (NOW() + INTERVAL 32 SECOND), NOW())
+, (24, 1, 4, 4, 'ゲームも終わりますね', (NOW() + INTERVAL 33 SECOND), NOW())
+, (25, 1, 4, 5, 'そうですね！', (NOW() + INTERVAL 34 SECOND), NOW())
 ;
