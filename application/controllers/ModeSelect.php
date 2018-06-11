71fe3696f87c950cd9361c1eef8cea247bd484d4
yamafuji <yamafuji@aiming-inc.com>
2018-06-11 17:08:56 +0900

    ログイン直後のモード選択画面の追加

--- application/controllers/ModeSelect.php
+++ application/controllers/ModeSelect.php
@@ -1,26 +1,55 @@
 <?php
+/*!
+ * ゲームモードの選択画面（ログイン必須）
+ *
+ * @user_id：ログインしているユーザーID
+ */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ModeSelect extends CI_Controller {
+/*!
+ */
+class ModeSelect extends CI_Controller
+{
+	private $_user_id;
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	/*!
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Useraccount_model', 'users');
+	}
+	
+	/*!
+	 */
+	private function _validate()
+	{
+   		$this->_user_id = $this->input->get('user_id');
+		if (NULL == $this->_user_id) {
+			throw new Exception('invalid params user_id');
+		}
+	}
+
+	/*!
 	 */
 	public function index()
 	{
-		$this->load->view('ModeSelect.php');
+		try {
+			$this->_validate();
+		} catch (Exception $obj) {
+			$url = sprintf("Login?error=%d", LOGIN_ERR_PARAMETER);
+			redirect($url);
+		}
+
+		$user = $this->users->load($this->_user_id);
+		if (NULL === $user) {
+			redirect("Login");
+			return ;
+		}
+
+		$data["user"] = $user;
+		$data["user_id"] = $this->_user_id;
+		$this->smarty->view('ModeSelect.tpl', $data);
 	}
 }
 
---
+++ application/views/templates/ModeSelect.tpl
@@ -0,0 +1,17 @@
+{strip}
+<div>
+{$user->nickname}さん、ようこそ！
+</div>
+<div>
+  <a href="{$SITE_URL}GameSelect?user_id={$user_id}">既存のゲームに参加</a>
+</div>
+<div>
+  <a href="{$SITE_URL}GameCreate?user_id={$user_id}">ゲームを新しく始める</a>
+</div>
+<div>
+  <a href="{$SITE_URL}GameHistory?user_id={$user_id}">過去の結果</a>
+</div>
+<div>
+  <a href="{$SITE_URL}Login">ログアウト</a>
+</div>
+{/strip}
