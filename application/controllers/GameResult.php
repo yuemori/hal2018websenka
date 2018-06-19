eb037aa95b1b8f6524959516d5cc0c770bfffe00
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-06-19 12:50:40 +0900

    ゲームの結果を表示する画面の追加

--- application/controllers/GameResult.php
+++ application/controllers/GameResult.php
@@ -1,25 +1,58 @@
 <?php
+/*!
+ * ゲームの結果画面
+ *
+ * user_id: ログイン中のユーザーID
+ * game_id: 進行中のゲームID
+ *
+ */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class GameResult extends CI_Controller {
+class GameResult extends CI_Controller
+{
+	private $_user_id;
+	private $_game_id;
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Game_model', 'game');
+	}
+
+	private function _validate()
+	{
+   		$this->_user_id = $this->input->get('user_id');
+		if (NULL == $this->_user_id) {
+			throw new Exception('invalid params user_id');
+		}
+		$this->_game_id = $this->input->get('game_id');
+		if (NULL == $this->_game_id) {
+			throw new Exception('invalid params game_id');
+		}
+	}
+
 	public function index()
 	{
-		$this->load->view('GameResult.php');
+		try {
+			$this->_validate();
+		} catch (Exception $obj) {
+			var_dump($obj);
+		}
+
+		$game = $this->game->load($this->_game_id);
+		if (NULL !== $game) {
+			$data["game"] = $game;
+			$data["is_wordwolf_win"] = $game->isWordwolfWin();
+		} else {
+			$data["game"] = NULL;
+			$data["is_wordwolf_win"] = false;
+		}
+
+
+		$data["user_id"] = $this->_user_id;
+		$data["game_id"] = $this->_game_id;
+		$this->smarty->view("GameResult.tpl", $data);
 	}
 }
+
+?>
