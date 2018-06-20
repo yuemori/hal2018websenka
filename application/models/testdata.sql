f518a747ff9eec9e5017000ad3211835d608b461
山藤智之 <yamafuji@yamafuji-mbp.local>
2018-06-20 16:48:44 +0900

    modify testdata

--- application/models/testdata.sql
+++ application/models/testdata.sql
@@ -42,7 +42,9 @@ DELETE FROM Game;
 INSERT INTO Game
   (game_id, status, group_id, minority_user_id, minimum, start_at, end_at)
 VALUES
-  (1, 0, 1, 1, 4, (NOW() -  INTERVAL 1400 SECOND), (NOW() +  INTERVAL 30 SECOND))
+  (1, 1, 1, 1, 4, (NOW() -  INTERVAL 1400 SECOND), (NOW() +  INTERVAL 30 SECOND))
+, (2, 0, 2, 0, 3, NULL, NULL)
+, (3, 0, 3, 0, 3, NULL, NULL)
 ;
 
 
